dce1e87446248e15ebd6b73d924e3643bd3a258f
niconicsss <baxalmonico2003@gmail.com>
2025-04-30 13:22:10 +0800

    place-order

--- orders/place-order.php
+++ orders/place-order.php
@@ -2,13 +2,11 @@
 include('../includes/auth.php');
 include('../config/db.php');
 
-// Check if data is posted
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $business_id = $_POST['business_id'];
     $order_details = $_POST['order_details'];
-    $user_id = $_SESSION['user_id']; // Make sure this is set in your auth session
+    $user_id = $_SESSION['user_id'];
 
-    // Insert order into the database
     $sql = "INSERT INTO orders (user_id, business_id, order_details, created_at) VALUES (?, ?, ?, NOW())";
     $stmt = $pdo->prepare($sql);
     
