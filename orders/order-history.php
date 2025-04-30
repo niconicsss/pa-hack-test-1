73ac6e8a5c44db05282d5447bae88eb1d6f2e695
althealastimosa <thealastimosa73@gmail.com>
2025-04-30 20:41:57 +0800

    order-history

--- orders/order-history.php
+++ orders/order-history.php
@@ -25,7 +25,7 @@ $orders = $stmt->fetchAll();
 <head>
     <meta charset="UTF-8">
     <title>Your Order History</title>
-    <link rel="stylesheet" href="styles/order-status.css">
+    <link rel="stylesheet" href="../styles/order-status.css">
 </head>
 <body>
     
